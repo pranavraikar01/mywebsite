8b531f1488342692cf52b4e986fa519450563627
PRANAV RAIKAR <pranavraikar01@gmail.com>
2023-04-17 23:00:45 +0530

    added database sqlite

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post,Author,Tag 
+
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    
+    prepopulated_fields={"slug":("title",)}#IT AUTOMATICALLY CALLS SLUGIFY WHICH IS INBUILT
+    list_filter = ("author", "tags","date",)     #ADDS FILTER SECTION WHICH INCLUDES THE INPUTS PASSED AS A TUPPLE HERE author and rating
+    list_display = ("title","date", "author",)     #displays in a better format WHICH INCLUDES THE INPUTS PASSED AS A TUPPLE HERE author and rating
+
+
+admin.site.register(Post,PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
