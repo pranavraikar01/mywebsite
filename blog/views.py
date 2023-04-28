20e1e6c0cde9abe055ed3b55d3d0f582ca750aeb
PRANAV RAIKAR <pranavraikar01@gmail.com>
2023-04-28 18:36:06 +0530

    user can write comment for a specific post

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import ListView
+from django.views import View
 
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -24,12 +28,31 @@ class AllPostsView(ListView):
     context_object_name = "all_posts"
 
 
-class SinglePostView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+class SinglePostView(View):
+    
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+          "post": post,
+          "post_tags": post.tags.all(),
+          "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
 
-    #this function is for adding tags
-    def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        return context
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)  #take input from the form
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+          comment = comment_form.save(commit=False) #creates a new model instance in the database
+          comment.post = post #adds extra data
+          comment.save()  #and saves the data back to database
+          
+          return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+        context = {
+          "post": post,
+          "post_tags": post.tags.all(),
+          "comment_form": comment_form
+        }
+        return render(request, "blog/post-detail.html", context)
